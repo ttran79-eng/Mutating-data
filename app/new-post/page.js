a7a969039e771389619ce03e82d67acd9567446d
YawnJawn <ttran.96@hotmail.com>
2025-05-26 13:30:27 -0500

    Added server side validation with error handling

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,15 +1,34 @@
-import FormSubmit from "@/components/form-submit";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
+import PostForm from "@/components/post-form";
 
 export default function NewPostPage() {
 
-  async function createPost(formData){
+  async function createPost(prevState, formData){
     "use server";
 
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
+    let errors = [];
+
+    if(!title || title.trim().length === 0){
+      errors.push("Title is required.");
+
+    }
+
+       if(!content || content.trim().length === 0){
+        errors.push("Content is required.");
+    }
+
+       if(!image || image.size === 0){
+        errors.push("Image is required.");
+      
+    }
+
+    if(errors.length > 0){
+      return { errors };
+    } 
 
     // The const title is named the same as the database so we can use the shortcut here...
     // Currently no path to image
@@ -23,30 +42,5 @@ export default function NewPostPage() {
     redirect('/feed');
 
   }
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <FormSubmit />
-      </form>
-    </>
-  );
+return <PostForm action={createPost} />;
 }
