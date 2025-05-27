35a0435750908d578285fe60a5f4165f59a17852
YawnJawn <ttran.96@hotmail.com>
2025-05-27 08:32:50 -0500

    Image issue resolved

--- app/actions/posts.js
+++ app/actions/posts.js
@@ -1,6 +1,7 @@
 "use server";
 import { redirect } from "next/navigation";
 import { storePost } from "@/lib/posts";
+import { uploadImage } from "@/lib/cloudinary";
 
 export async function createPost(prevState, formData){
 
